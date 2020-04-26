9a394b854d8c6199ee4007608c867b8046deacfe
Ihor Stakhiv <ihorstakhivs@gmail.com>
2020-04-26 17:56:03 +0300

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
+  root 'static_pages#home'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
@@ -6,6 +10,4 @@ Rails.application.routes.draw do
   get  'static_pages/about'
 
   get  'static_pages/contact'
-
-  root 'static_pages#home'
 end
